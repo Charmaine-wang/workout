1bc3f867f487a171cb8deb19666172548095ac9d
Sofia Garcia <sofgar0614@gmail.com>
2019-11-14 02:08:53 +0100

    added last 30 days calendar loop

--- src/components/StopTimer.js
+++ src/components/StopTimer.js
@@ -4,26 +4,19 @@ import { useAuth } from '../authcontext';
 
 const StyledStopTimer = styled.div`
 	display: flex;
-
 	opacity: ${props => (props.showStopBtn ? "1" : "0")};
+	/* opacity: 1; */
 	flex-direction: column;
 	align-items: center;
 	justify-content: center;
 	position: absolute;
 	top: 225px;
 	left: 65vw;
-	transition: 1s;
+	transition: ${props => (props.showStopBtn ? "0.7s" : "0.7s")};
 	border-radius: 50%;
-
-	background-color: ${props =>
-		props.startedBg ? "rgba(0,0,0, 0.6)" : "rgba(130,0,0, 0.9)"};
-	/* byt färg till nått nice, eller gör andra animationer */
-	border: ${props =>
-		props.startedBg ? "12px solid black" : "0px solid rgba(50,0,0, 0.1)"};
-
+	background-color: rgba(100,0,0, 1);
 	font-size: 18px;
 	color: white;
-
 	width: ${props => (props.showStopBtn ? "100px" : "70px")};
 	height: ${props => (props.showStopBtn ? "100px" : "70px")};
 `;
