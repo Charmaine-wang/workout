733353a1b324550e273623e22180f52d335950fe
charmaine wang <charmainewang424@hotmail.com>
2019-11-15 12:41:07 +0100

    reset button

--- src/components/TimeKeeperComponent.js
+++ src/components/TimeKeeperComponent.js
@@ -89,7 +89,7 @@ const TimeKeeperComponent = (props) => {
 
 		if (navigator.geolocation) {
 			navigator.geolocation.getCurrentPosition( position => {
-			const currentLocation = position.coords;
+			const currentLocation = position.coords
 			let calcDist = null;
 
 			if (currentLocation && prevPosition) {
@@ -120,15 +120,15 @@ const TimeKeeperComponent = (props) => {
 	let minutes = ("0" + (Math.floor(seconds / 60) % 60)).slice(-2);
 	let hours = ("0" + Math.floor(seconds / 360)).slice(-2);
 
-	// const days = [
-	// 	"Sun",
-	// 	"Mon",
-	// 	"Tue",
-	// 	"Wed",
-	// 	"Thu",
-	// 	"Fri",
-	// 	"Sat"
-	// ];
+	const days = [
+		"Mon",
+		"Tue",
+		"Wed",
+		"Thu",
+		"Fri",
+		"Sat",
+		"Sun"
+	];
 
 	const monthNames = [
 		"January",
@@ -147,12 +147,14 @@ const TimeKeeperComponent = (props) => {
 
   const date = new Date()
 
-	// const day = days[date.getDay()]
+	const day = days[date.getDay()]
 	const dateNum = date.getDate()
 	const month = monthNames[date.getMonth()]
 
 	const fetchWorkout = event => {
+		// toggleTimer();
 		setIsActive(false);
+		setSeconds(0);
 		firebase
 			.firestore()
 			.collection("users")
@@ -164,9 +166,9 @@ const TimeKeeperComponent = (props) => {
 			   activitytime: { minutes, seconds },
 			   kcal: caloriesBurned,
 			   distance: totalDistanceRounded,
-				 // day: day,
+				 day: day,
 				 dateNum: dateNum,
-         month: month
+         month: month,
 			});
 	};
 
@@ -223,8 +225,10 @@ const TimeKeeperComponent = (props) => {
 	return (
 		<StyledTimekeeperComponent expanded={props.isToggled} onSubmit={isActive}>
 			<ArrowBack {...props} onClick={() => {
-				totalDistanceRounded >= 0.01 && fetchWorkout()
-			 	props.goBack()
+				totalDistanceRounded >= 1 && fetchWorkout();
+				 props.goBack()
+				 setSeconds(0)
+				 toggleTimer()
 			}
 			 }>
 				<img src="/images/arrowBack.png" alt="arrow back" />
